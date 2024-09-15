e0b546725ebc23c9dc2bcba42b413a36e45cbac4
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-09-16 02:26:17 +0530

    Update JobDetails.jsx

--- src/components/Job/JobDetails.jsx
+++ src/components/Job/JobDetails.jsx
@@ -12,7 +12,7 @@ const JobDetails = () => {
 
   useEffect(() => {
     axios
-      .get(`http://localhost:4000/api/v1/job/${id}`, {
+      .get(`https://job-backend-sm20.onrender.com/api/v1/job/${id}`, {
         withCredentials: true,
       })
       .then((res) => {
