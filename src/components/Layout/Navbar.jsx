c109abbcc66b3c86deef5eb996c339b72c80b723
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-09-16 02:28:24 +0530

    Update Navbar.jsx

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
   const handleLogout = async () => {
     try {
       const response = await axios.get(
-        "http://localhost:4000/api/v1/user/logout",
+        "https://job-backend-sm20.onrender.com/api/v1/user/logout",
         {
           withCredentials: true,
         }
