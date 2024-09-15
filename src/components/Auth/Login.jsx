41034c1830471a158f3f061329d423366685fa37
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2024-09-16 02:25:16 +0530

    Update Login.jsx

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -18,7 +18,7 @@ const Login = () => {
     e.preventDefault();
     try {
       const { data } = await axios.post(
-        "http://localhost:4000/api/v1/user/login",
+        "https://job-backend-sm20.onrender.com/api/v1/user/login",
         { email, password, role },
         {
           headers: {
