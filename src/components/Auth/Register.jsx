8754d601e1fac00ff63038c82220f4febbe245e0
Aman Shyam Verma <92292381+wizard-aka-aman@users.noreply.github.com>
2025-04-29 22:03:58 +0530

    Update Register.jsx
    change in placeholder

--- src/components/Auth/Register.jsx
+++ src/components/Auth/Register.jsx
@@ -73,7 +73,7 @@ const Register = () => {
               <div>
                 <input
                   type="text"
-                  placeholder="Zeeshan"
+                  placeholder="Name"
                   value={name}
                   onChange={(e) => setName(e.target.value)}
                 />
@@ -85,7 +85,7 @@ const Register = () => {
               <div>
                 <input
                   type="email"
-                  placeholder="zk@gmail.com"
+                  placeholder="example@gmail.com"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
                 />
